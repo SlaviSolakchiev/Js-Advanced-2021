40a3d850c4550b517cc9a8c4da18a766aaad3769
SlaviSolakchiev <78353852+SlaviSolakchiev@users.noreply.github.com>
2023-10-19 17:12:08 +0300

    resolved

--- 01 Advanecd Syntax Functions and Statements/01 Lab/02_stringLength.js
+++ 01 Advanecd Syntax Functions and Statements/01 Lab/02_stringLength.js
@@ -1,5 +1,7 @@
 function foo(...args) {
-    const length = args.reduce((a, v) => a + v.length, 0)
-    return `${length}
-${Math.round(length / args.length)}`
+    const length = args.reduce((a, x) => a + x.length, 0)
+    console.log(`${length}
+${Math.round(length / args.length)}`)
 }
+
+foo('chocolate', 'ice cream', 'cake')
