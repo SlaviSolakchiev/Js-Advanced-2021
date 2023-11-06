e81696ee833933a5e624b225966ba7333e8d2368
SlaviSolakchiev <78353852+SlaviSolakchiev@users.noreply.github.com>
2023-11-06 14:08:32 +0200

    Create 03_carFactory.js

---
+++ 03 Objects and Composition/02 Exercise/03_carFactory.js
@@ -0,0 +1,36 @@
+function foo({ model, power, color, carriage, wheelsize }) {
+    const storageEngines = [
+        { power: 90, volume: 1800 },
+        { power: 120, volume: 2400 },
+        { power: 200, volume: 3500 },
+    ]
+    const storageCarriage = [
+        { type: "hatchback", color: "" },
+        { type: "coupe", color: "" },
+    ]
+    const getWheels = s => {
+        const arr = []
+        arr.length = 4
+        return s % 2 === 0 ? arr.fill(s - 1) : arr.fill(s)
+    }
+
+    console.log({
+        model,
+        engine: storageEngines.reduce((a, v) =>
+            Math.abs(a.power - power) < Math.abs(v.power - power) ? a : v
+        ),
+        carriage: { type: carriage, color },
+        wheels: getWheels(wheelsize),
+    })
+}
+
+
+
+foo({
+    model: 'VW Golf II',
+    power: 90,
+    color: 'blue',
+    carriage: 'hatchback',
+    wheelsize: 14
+}
+);
