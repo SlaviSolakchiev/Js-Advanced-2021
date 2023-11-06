a32b0aa142d4b14fc6ed5a75f2ebb1dbf89df59b
SlaviSolakchiev <78353852+SlaviSolakchiev@users.noreply.github.com>
2023-11-06 14:08:00 +0200

    Create 01_cityRecord.js

---
+++ 03 Objects and Composition/01 Lab/01_cityRecord.js
@@ -0,0 +1,16 @@
+function solve(name, population, treasury) {
+
+    let city = {
+        name: name,
+        population: population,
+        treasury: treasury,
+    }
+
+    return city;
+}
+
+
+solve('Tortuga',
+    7000,
+    15000
+);
